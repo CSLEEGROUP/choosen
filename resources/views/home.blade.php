1e971ac954f95ee388ecbbaa6a3de5fd1169218d
cslee7389 <cslee@squincy.com>
2017-07-21 23:54:38 +0800

    make it better

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,15 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Hi {{ Auth::user()->email }}, You are logged in!</div>
+                <div class="panel-heading">Hi {{ Auth::user()->name }}, You are logged in!</div>
 
                 <div class="panel-body">
-                    
+                        <div class="media">
+                                <div class="media-body">
+                                    <iframe width="100%" height="350" src="https://www.youtube.com/embed/APvHaKT3CuI" frameborder="0" allowfullscreen>
+                                    </iframe>
+                                </div>
+                        </div>
                 </div>
             </div>
         </div>
